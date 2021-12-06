af759ec01e5a03eb1b568eeec6406a6e769e2875
Britney Gwynne <britneygwynne@MacBook-Pro.modem>
2021-12-06 15:52:07 +1000

    finished etch-a-sketch: added transparency, slider

--- etch-A-Sketch/js.js
+++ etch-A-Sketch/js.js
@@ -1,11 +1,13 @@
 let gridSize = 16;
 let gridWidth = 600;
 let type = 'rainbow'
-let color = 'red';
+let color;
+let alpha = 0;
 
 const grid = document.querySelector('.grid');
 const buttons = document.querySelectorAll('button');
 const allCol = document.querySelectorAll('.colGridSpace');
+const slider = document.querySelector('#myRange');
 createGrid();
 colorEventListeners();
 let allGrid = document.querySelectorAll('.gridSpace');
@@ -16,15 +18,31 @@ const sizeText = document.querySelector('.sizing');
 sizeText.textContent = `${gridSize} x ${gridSize}`;
 
 function gridEvents(){
+    allGrid.forEach((space) => { 
+        space.alphAtt = 1;
+    });
     allGrid.forEach((space) => {
         space.addEventListener('mouseover', () => {
+            if(alpha == 0){
+                space.alphAtt = 1;
+            }
             if(type == 'rainbow'){
-                space.style.backgroundColor = `rgb(${random(0, 255)}, ${random(0, 255)}, ${random(0, 255)})`;
+                if(space.alphAtt > 0){
+                    space.alphAtt -= alpha;
+                }
+                space.style.backgroundColor = `rgba(${random(0, 255)}, ${random(0, 255)}, ${random(0, 255)}, ${space.alphAtt})`;
             } else if (type == 'colours'){
-                space.style.backgroundColor = color;
+                let temp;
+                if(space.alphAtt > 0){
+                    space.alphAtt -= alpha;
+                    temp = colorConverter(space.alphAtt);
+                }
+                space.style.backgroundColor = temp;
+                
             } else if (type == 'eraser'){
                 space.style.backgroundColor = 'rgb(230, 230, 230)';
             }
+            
         });
     });
 }
@@ -50,52 +68,53 @@ function colorEventListeners(){
             type = 'colours';
             color = clr.id;
             allCol.forEach((col) => col.classList.remove('colActive'));
-            buttons.forEach((button) => button.classList.remove('active'));
+            buttons.forEach((button) => {
+                if(button.id != 'transparent')
+                {button.classList.remove('active')}
+            });
             clr.classList.add('colActive');
-            colorConverter();
+            
         });
     });
 }
 
-function colorConverter(){
+function colorConverter(space){
     switch(color){
         case 'red':
-            color = "rgb(204, 0, 0)";
-            break;
+            return `rgba(204, 0, 0, ${space})`;
         case 'orange':
-            color = "rgb(238, 156, 4)";
-            break;
+            return `rgba(238, 156, 4, ${space})`;
         case 'yellow':
-            color = "rgb(238, 234, 4)";
-            break;
+            return `rgba(238, 234, 4, ${space})`;
         case 'darkGreen':
-            color = "rgb(1, 129, 8)";
-            break;
+            return `rgba(1, 129, 8, ${space})`;
         case 'lightGreen':
-            color = "rgb(114, 235, 110)";
-            break;
+            return `rgba(114, 235, 110, ${space})`;
         case 'lightBlue':
-            color = "rgb(4, 238, 226)";
-            break;
+            return `rgba(4, 238, 226, ${space})`;
         case 'darkBlue':
-            color = "rgb(15, 0, 219)";
-            break;
+            return `rgba(15, 0, 219, ${space})`;
         case 'purple':
-            color = "rgb(144, 4, 238)";
-            break;
+            return `rgba(144, 4, 238, ${space})`;
         case 'pink':
-            color = "rgb(219, 10, 219)";
+            return `rgba(219, 10, 219, ${space})`;
     }
+
 }
 
 function buttonEvents(){
     const rainbow = document.querySelector('#rainbow');
     const clear = document.querySelector('#clear');
     const eraser = document.querySelector('#eraser');
+    const transparent = document.querySelector('#transparent');
 
     rainbow.addEventListener('click', () => {
         type = 'rainbow';
-        buttons.forEach((button) => button.classList.remove('active'));
+        buttons.forEach((button) => {
+            if(button.id != 'transparent'){
+                button.classList.remove('active')
+            }
+        });
         allCol.forEach((col) => col.classList.remove('colActive'));
         rainbow.classList.add('active');
     });
@@ -103,13 +122,25 @@ function buttonEvents(){
         type = 'eraser'
         buttons.forEach((button) => button.classList.remove('active'));
         allCol.forEach((col) => col.classList.remove('colActive'));
+        allGrid.forEach((space) => space.alphAtt = 1);
         eraser.classList.add('active');
     });
+    transparent.addEventListener('click', () => {
+        transparent.classList.toggle('active');
+        if(alpha == 0.1){
+            alpha = 0;
+        } else if(alpha == 0){
+            alpha = 0.1;
+        }
+    })
     clear.addEventListener('click', reset);
+    //reset if slider changes
+    slider.oninput = reset;
+
 } 
 
 function reset(){
-    answer = prompt("How many squares do you want the grid to be wide? (minimum 10, maximum 100)");
+    answer = slider.value;
     if(!isNaN(answer) && (answer >= 10 && answer <= 100)){
         while(grid.firstChild){
             grid.removeChild(grid.firstChild);
