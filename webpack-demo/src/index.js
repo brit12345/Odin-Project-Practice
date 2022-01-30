3c3194f472f2157cac84244a1cb376b207f1e8f6
Britney Gwynne <britneygwynne@MacBook-Pro.modem>
2022-01-30 17:03:24 +1000

    progress on webpack project

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,5 +1,9 @@
 import _ from 'lodash';
 import myName from './myName.js';
+import './style.css';
+import Icon from './icon.png';
+import Data from './data.xml';
+import Notes from './data.csv';
 
 function component() {
   const element = document.createElement('div');
@@ -7,6 +11,16 @@ function component() {
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  //add the image to our existing div
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
+
+  console.log(Data);
+  console.log(Notes);
 
   return element;
 }
